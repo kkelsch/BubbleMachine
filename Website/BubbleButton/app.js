0f299c0c4baf625e207a2928e485601c8383c108
unknown <kelschjunk@gmail.com>
2018-09-30 15:53:02 -0500

    added pubnub logic

--- Website/BubbleButton/app.js
+++ Website/BubbleButton/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,6 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
