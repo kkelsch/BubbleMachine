6dae4c1587b72a544a6f6ed5098176a9c46c9c95
unknown <kelschjunk@gmail.com>
2018-09-30 16:22:53 -0500

    updated button styles

--- Website/BubbleButton/routes/index.js
+++ Website/BubbleButton/routes/index.js
@@ -3,13 +3,9 @@ var router = express.Router();
 var PubNub = require('pubnub');
 module.exports = router;
 
-function buttonClick(){
-  alert('test');
-}
-
 // SUPER SECRET SHIT
-var pubKey = '<secret>';
-var subKey = '<secret>';
+var pubKey = 'pub-c-1b8f92cc-6dbb-47f0-b329-bcf297c85cda';
+var subKey = 'sub-c-b15b8210-bea3-11e8-8fd2-4a2bdf4876be';
 var channelName = 'Bubble-Channel'; 
 
 var pubnub = new PubNub({
