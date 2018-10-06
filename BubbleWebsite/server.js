79b18e08cc3b03bd81c3cf97f43c7259756874e9
unknown <kelschjunk@gmail.com>
2018-10-06 17:35:49 -0500

    bubble site working!

---
+++ BubbleWebsite/server.js
@@ -0,0 +1,48 @@
+var express =require('express');
+var app = express();
+var port=process.env.PORT || 3000;
+var PubNub = require('pubnub');
+
+// SUPER SECRET SHIT
+var pubKey = 'pub-c-1b8f92cc-6dbb-47f0-b329-bcf297c85cda';
+var subKey = 'sub-c-b15b8210-bea3-11e8-8fd2-4a2bdf4876be';
+var channelName = 'Bubble-Channel'; 
+
+var pubnub = new PubNub({
+    publishKey : pubKey,
+    subscribeKey : subKey
+  });
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+console.log('hello from server');
+ res.render('./public/index.html');
+});
+
+// call from front end
+app.get('/sendMessage', function(req, res) {
+    publishMessage(); 
+});
+
+// Publish Message
+function publishMessage() {
+    console.log("Sending message....");
+    var publishConfig = {
+        channel : channelName,
+        message : {
+            title: "Hype Message",
+            description: "ENGAGE HYPE"
+        }
+    };
+    pubnub.publish(publishConfig, function(status, response) {
+        console.log(status, response);
+        
+    });
+
+};
+
+
+app.listen(port);
+console.log('Server Listening at port'+port);
