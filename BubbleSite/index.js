890ae526a5a47abed68d55ca3a841ca0942f5dfb
unknown <kelschjunk@gmail.com>
2018-10-06 13:25:08 -0500

    BIG RED BUTTON

--- BubbleSite/index.js
+++ BubbleSite/index.js
@@ -1,13 +1,33 @@
-var http = require('http');
+// var http = require('http');
+// //var express = require('express');
+// //const app = new express();
+// var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
+// var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write(html);  
+//     response.end();  
 
+// });
+
+// fs.readFile('./index.html', function (err, html) {
+
+
+
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(8080);
