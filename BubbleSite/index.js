86ed0387f73ddf195be5feeb224786977f82d890
unknown <kelschjunk@gmail.com>
2018-10-06 13:52:31 -0500

    added back pub nub

--- BubbleSite/index.js
+++ BubbleSite/index.js
@@ -1,33 +1,51 @@
-// var http = require('http');
-// //var express = require('express');
-// //const app = new express();
-// var fs = require('fs');
 
-// var server = http.createServer(function(request, response) {
-
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.write(html);  
-//     response.end();  
-
-// });
-
-// fs.readFile('./index.html', function (err, html) {
-
-
-
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
 var express = require('express');
 var app = express();
 var path = require('path');
+var PubNub = require('pubnub');
 
+// for style sheets
 app.use(express.static(__dirname + '/public'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
-// viewed at http://localhost:8080
+// SUPER SECRET SHIT
+var pubKey = 'pub-c-1b8f92cc-6dbb-47f0-b329-bcf297c85cda';
+var subKey = 'sub-c-b15b8210-bea3-11e8-8fd2-4a2bdf4876be';
+var channelName = 'Bubble-Channel'; 
+
+var pubnub = new PubNub({
+    publishKey : pubKey,
+    subscribeKey : subKey
+  });
+
+
+
+// endpoints
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
+app.get('/sendMessage', function(req, res) {
+    publishMessage(); 
+});
+
+// Publish Message
+function publishMessage() {
+    console.log("Sending message");
+    var publishConfig = {
+        channel : channelName,
+        message : {
+            title: "Hype Message",
+            description: "ENGAGE HYPE"
+        }
+    };
+    pubnub.publish(publishConfig, function(status, response) {
+        console.log(status, response);
+        
+    });
+
+};
+
+console.log('Starting Site...');
 app.listen(8080);
